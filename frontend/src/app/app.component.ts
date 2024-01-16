87b58ac2c8f815503ccdd2482084da2bef4d6f63
gaurav <gaurav@zendynamix.com>
2024-01-16 13:14:25 +0530

    Project Frontend Initialised

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'vehicle-rent-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'frontend';
+}
